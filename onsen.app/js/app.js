e7f8d380d2be530e97238a0422207a4c09cbcd0f
KARIN PIMLOY (กรินทร์ พิมพ์ลอย) <6030213023@email.psu.ac.th>
2019-09-17 00:39:41 +0700

    sucess

--- onsen.app/js/app.js
+++ onsen.app/js/app.js
@@ -2,27 +2,27 @@ document.addEventListener('init', function(event) {
     var page = event.target;
 console.log(page.id);
 
-    if (page.id ==="tabbar") {
+    if (page.id ==='tabbar') {
         //code for tapbar
         
         $("#menubtn").click(function(){
         var menu = document.getElementById('menu');
         menu.open();
     });
+    $("#btnFastfood").click(function(){
+
+        var content = document.getElementById('content');
+          var menu = document.getElementById('menu');
+        content.load('fastfood.html')
+         .then(menu.close.bind(menu));
+    });
     }
 
-    if (page.id ==='tab1') {
-        //code for tap1
-        
-        $("#btn1").click(function(){
-
-            ons.notification.alert("hello");
-        });
-    }
+    
     if (page.id ==="sidemenu") {
         //code for tap1
           
-        $("#loginbtn").click(function(){
+        $("#logoutbtn").click(function(){
 
             var content = document.getElementById('content');
               var menu = document.getElementById('menu');
@@ -62,7 +62,7 @@ console.log(page.id);
         });
         $("#btnLogin").click(function(){
 
-            // ons.notification.alert("Login Complete");
+         
           
 
                 var content = document.getElementById('content');
@@ -88,10 +88,62 @@ console.log(page.id);
             });
 
         }
-    if(page.id==='tabbar'){
-       
-    }
+        if (page.id ==='fastfood') {
+         
+            $("#back").click(function(){
 
+                var content = document.getElementById('content');
+                var menu = document.getElementById('menu');
+                content.load('tabbar.html')
+                .then(menu.close.bind(menu));
+               
+            });
+            $("#btnKfc").click(function(){
+
+                var content = document.getElementById('content');
+                var menu = document.getElementById('menu');
+                content.load('MenuKfc.html')
+                .then(menu.close.bind(menu));
+               
+            });
+
+        }
+        if (page.id ==='Kfc') {
+         
+            $("#back").click(function(){
+
+                var content = document.getElementById('content');
+                var menu = document.getElementById('menu');
+                content.load('fastfood.html')
+                .then(menu.close.bind(menu));
+               
+            });
+        
+            $("#btnOrder").click(function(){
+
+                var content = document.getElementById('content');
+                var menu = document.getElementById('menu');
+                content.load('orderCf.html')
+                .then(menu.close.bind(menu));
+               
+            });
+
+        }
+        if (page.id ==='orderCf') {
+         
+            $("#back").click(function(){
+
+                var content = document.getElementById('content');
+                var menu = document.getElementById('menu');
+                content.load('MenuKfc.html')
+                .then(menu.close.bind(menu));
+               
+            });
+        
+           
+
+        }
+    
 
 
 
