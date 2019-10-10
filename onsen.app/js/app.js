40e197cd4cf7a6fcdf6fc86183a51f1c8607ff2a
KARIN PIMLOY (กรินทร์ พิมพ์ลอย) <6030213023@email.psu.ac.th>
2019-10-10 23:13:51 +0700

    editUI+function

--- onsen.app/js/app.js
+++ onsen.app/js/app.js
@@ -1,5 +1,34 @@
+
+//  // Your web app's Firebase configuration
+//  var firebaseConfig = {
+//     apiKey: "AIzaSyDrcRZdk6aaySis7IfPWg-zJDk-BL__c_s",
+//     authDomain: "food-1ef28.firebaseapp.com",
+//     databaseURL: "https://food-1ef28.firebaseio.com",
+//     projectId: "food-1ef28",
+//     storageBucket: "food-1ef28.appspot.com",
+//     messagingSenderId: "608405620855",
+//     appId: "1:608405620855:web:26d1be7043611fa50cde72"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+//         //use firestore
+//         var db = firebase.firestore();
+
+//         db.collection("recommended").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data()}`);
+//         console.log(doc.data().name);
+//         console.log(doc.data().id);
+//     });
+// });
+     
+    
 document.addEventListener('init', function(event) {
+
     var page = event.target;
+
+
 console.log(page.id);
 
     if (page.id ==='tabbar') {
@@ -15,7 +44,35 @@ console.log(page.id);
           var menu = document.getElementById('menu');
         content.load('fastfood.html')
          .then(menu.close.bind(menu));
+
     });
+    $("#Backhome").click(function(){
+
+        var content = document.getElementById('content');
+        var menu = document.getElementById('menu');
+        content.load('tabbar.html')
+        .then(menu.close.bind(menu));
+       
+    });
+//     $("#carousel").empty();
+          
+//     db.collection("recommended").get().then((querySnapshot) => {
+//       querySnapshot.forEach((doc) => {
+         
+//         var item = `<ons-carousel-item modifier="nodivider" id="${doc.data().id}" class="recomended_item">
+//         <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
+//         </div>
+//         <div class="recomended_item_title" id="item1_name">${doc.data().name}</div>
+//     </ons-carousel-item>`
+
+//         $("#carousel").append(item);
+          
+//       });
+
+// });
+    
+  
+      
     }
 
     
@@ -98,6 +155,15 @@ console.log(page.id);
                 .then(menu.close.bind(menu));
                
             });
+            
+            $("#Backhome").click(function(){
+
+                var content = document.getElementById('content');
+                var menu = document.getElementById('menu');
+                content.load('tabbar.html')
+                .then(menu.close.bind(menu));
+               
+            });
             $("#btnKfc").click(function(){
 
                 var content = document.getElementById('content');
@@ -118,7 +184,14 @@ console.log(page.id);
                 .then(menu.close.bind(menu));
                
             });
-        
+            $("#Backhome").click(function(){
+
+                var content = document.getElementById('content');
+                var menu = document.getElementById('menu');
+                content.load('tabbar.html')
+                .then(menu.close.bind(menu));
+               
+            });
             $("#btnOrder").click(function(){
 
                 var content = document.getElementById('content');
@@ -140,7 +213,14 @@ console.log(page.id);
                
             });
         
-           
+            $("#Backhome").click(function(){
+
+                var content = document.getElementById('content');
+                var menu = document.getElementById('menu');
+                content.load('tabbar.html')
+                .then(menu.close.bind(menu));
+               
+            });
 
         }
     
