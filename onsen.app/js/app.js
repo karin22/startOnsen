34a23486cc3d04a40594af4614021f27a642567c
KARIN PIMLOY (กรินทร์ พิมพ์ลอย) <6030213023@email.psu.ac.th>
2019-10-11 21:09:49 +0700

    firebasecode erorr ยังแก้ไม่ได้

--- onsen.app/js/app.js
+++ onsen.app/js/app.js
@@ -1,28 +1,17 @@
 
-//  // Your web app's Firebase configuration
-//  var firebaseConfig = {
-//     apiKey: "AIzaSyDrcRZdk6aaySis7IfPWg-zJDk-BL__c_s",
-//     authDomain: "food-1ef28.firebaseapp.com",
-//     databaseURL: "https://food-1ef28.firebaseio.com",
-//     projectId: "food-1ef28",
-//     storageBucket: "food-1ef28.appspot.com",
-//     messagingSenderId: "608405620855",
-//     appId: "1:608405620855:web:26d1be7043611fa50cde72"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-
-//         //use firestore
-//         var db = firebase.firestore();
-
-//         db.collection("recommended").get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//         console.log(`${doc.id} => ${doc.data()}`);
-//         console.log(doc.data().name);
-//         console.log(doc.data().id);
-//     });
-// });
-     
+       // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDrcRZdk6aaySis7IfPWg-zJDk-BL__c_s",
+    authDomain: "food-1ef28.firebaseapp.com",
+    databaseURL: "https://food-1ef28.firebaseio.com",
+    projectId: "food-1ef28",
+    storageBucket: "food-1ef28.appspot.com",
+    messagingSenderId: "608405620855",
+    appId: "1:608405620855:web:26d1be7043611fa50cde72"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore(); 
     
 document.addEventListener('init', function(event) {
 
@@ -54,22 +43,22 @@ console.log(page.id);
         .then(menu.close.bind(menu));
        
     });
-//     $("#carousel").empty();
+    $("#carousel").empty();
           
-//     db.collection("recommended").get().then((querySnapshot) => {
-//       querySnapshot.forEach((doc) => {
+    db.collection("recommended").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
          
-//         var item = `<ons-carousel-item modifier="nodivider" id="${doc.data().id}" class="recomended_item">
-//         <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
-//         </div>
-//         <div class="recomended_item_title" id="item1_name">${doc.data().name}</div>
-//     </ons-carousel-item>`
+        var item = `<ons-carousel-item modifier="nodivider" id="${doc.data().id}" class="recomended_item">
+        <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
+        </div>
+        <div class="recomended_item_title" id="item1_name">${doc.data().name}</div>
+    </ons-carousel-item>`
 
-//         $("#carousel").append(item);
+        $("#carousel").append(item);
           
-//       });
+      });
 
-// });
+});
     
   
       
